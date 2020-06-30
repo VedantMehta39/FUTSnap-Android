2485cf043a189e9b5a4e927e861140e4aec39700
VedantMehta39 <42540089+VedantMehta39@users.noreply.github.com>
2020-06-30 10:00:39 -0700

    Update DB Model and Room Config

--- app/src/main/java/com/example/futbinwatchernew/Database/FUTBINWatcherDatabase.kt
+++ app/src/main/java/com/example/futbinwatchernew/Database/FUTBINWatcherDatabase.kt
@@ -2,8 +2,12 @@ package com.example.futbinwatchernew.Database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import kotlinx.android.parcel.TypeParceler
 
 @Database(entities = [PlayerDBModel::class], version = 1)
+@TypeConverters(PlatformTypeConverter::class)
 abstract class FUTBINWatcherDatabase:RoomDatabase() {
 
     abstract fun getPlayerDao():PlayerDAO
--- app/src/main/java/com/example/futbinwatchernew/Database/PlayerDBModel.kt
+++ app/src/main/java/com/example/futbinwatchernew/Database/PlayerDBModel.kt
@@ -2,14 +2,17 @@ package com.example.futbinwatchernew.Database
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.futbinwatchernew.Models.Platform
 import com.example.futbinwatchernew.Network.ResponseModels.SearchPlayerResponse
 
 @Entity(tableName = "tracked_players")
-data class PlayerDBModel(@PrimaryKey val id:Int, val name:String, val imageURL:String,
-                         val rating:Int, var targetPrice:Int,
-                         var currentPrice:Int){
+data class PlayerDBModel(@PrimaryKey var id:Int, var name:String, var imageURL:String,
+                         var rating:Int, var targetPrice:Int,
+                         var currentPrice:Int, var platform: Platform?, var gte:Boolean, var lt:Boolean){
 
     constructor(data:SearchPlayerResponse, currentPlayerPrice: Int): this(data.id, data.playerName, data.playerImage,
-        data.playerRating, -1, currentPlayerPrice )
+        data.playerRating, -1, currentPlayerPrice, null,false,false )
+
+    constructor():this(-1,"","",-1,-1,-1,null,false,false)
 
 }
