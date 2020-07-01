79ac3f74a6ac74e40c7c86be56f677a32065ca94
VedantMehta39 <42540089+VedantMehta39@users.noreply.github.com>
2020-07-01 08:57:59 -0700

    Remove currenPrice from PlayerDBModel

--- app/src/main/java/com/example/futbinwatchernew/Database/PlayerDBModel.kt
+++ app/src/main/java/com/example/futbinwatchernew/Database/PlayerDBModel.kt
@@ -8,11 +8,11 @@ import com.example.futbinwatchernew.Network.ResponseModels.SearchPlayerResponse
 @Entity(tableName = "tracked_players")
 data class PlayerDBModel(@PrimaryKey var id:Int, var name:String, var imageURL:String,
                          var rating:Int, var targetPrice:Int,
-                         var currentPrice:Int, var platform: Platform?, var gte:Boolean, var lt:Boolean){
+                         var platform: Platform?, var gte:Boolean, var lt:Boolean){
 
-    constructor(data:SearchPlayerResponse, currentPlayerPrice: Int): this(data.id, data.playerName, data.playerImage,
-        data.playerRating, -1, currentPlayerPrice, null,false,false )
+    constructor(data:SearchPlayerResponse): this(data.id, data.playerName, data.playerImage,
+        data.playerRating, -1, null,false,false )
 
-    constructor():this(-1,"","",-1,-1,-1,null,false,false)
+    constructor():this(-1,"","",-1,-1,null,false,false)
 
 }
