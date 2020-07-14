ee9925ede25449a1cf9a6192bb6c1603d506733a
VedantMehta39 <42540089+VedantMehta39@users.noreply.github.com>
2020-07-14 03:12:21 -0700

    Redesign SearchPlayerViewModel.kt to have a single responsibility

--- app/src/main/java/com/example/futbinwatchernew/SearchPlayerViewModel.kt
+++ app/src/main/java/com/example/futbinwatchernew/SearchPlayerViewModel.kt
@@ -3,19 +3,11 @@ package com.example.futbinwatchernew
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.futbinwatchernew.Models.Platform
-import com.example.futbinwatchernew.Models.PlayerDialogFragModel
 import com.example.futbinwatchernew.Network.ApiClient
-import com.example.futbinwatchernew.Network.ResponseModels.PlayerPriceResponse
 import com.example.futbinwatchernew.Network.ResponseModels.SearchPlayerResponse
-import com.example.futbinwatchernew.Services.Models.PlayerTrackingRequest
-import com.example.futbinwatchernew.UI.Event
 import kotlinx.coroutines.launch
 import java.lang.Exception
-import java.util.*
 import javax.inject.Inject
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 class SearchPlayerViewModel():ViewModel() {
     var searchPlayersResult = MutableLiveData<List<SearchPlayerResponse>>()
