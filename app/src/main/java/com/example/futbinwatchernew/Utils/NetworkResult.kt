4b23c0dde8c8e73a0c235cda562cc634da3c5d43
VedantMehta39 <42540089+VedantMehta39@users.noreply.github.com>
2020-07-21 04:20:02 -0700

    Add a NetworkResult.kt wrapper for API responses

--- app/src/main/java/com/example/futbinwatchernew/Utils/NetworkResult.kt
+++ app/src/main/java/com/example/futbinwatchernew/Utils/NetworkResult.kt
@@ -1,6 +1,8 @@
 package com.example.futbinwatchernew.Utils
 
+import com.example.futbinwatchernew.UI.ErrorHandling.Error
+
 sealed class NetworkResponse<out T>{
     class Success<T>(val data: T):NetworkResponse<T>()
-    class Failure(val error:Error):NetworkResponse<Nothing>()
+    class Failure(val error: Error):NetworkResponse<Nothing>()
 }
