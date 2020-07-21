e488aec7411571262fad07e971e9e1f16a27b321
VedantMehta39 <42540089+VedantMehta39@users.noreply.github.com>
2020-07-21 04:04:10 -0700

    Add a Fragment for displaying errors

--- app/src/main/java/com/example/futbinwatchernew/UI/ErrorFragment.kt
+++ app/src/main/java/com/example/futbinwatchernew/UI/ErrorFragment.kt
@@ -1,5 +1,6 @@
 package com.example.futbinwatchernew.UI
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,13 +9,23 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.futbinwatchernew.FUTBINWatcherApp
+import com.example.futbinwatchernew.MainActivityViewModel
 import com.example.futbinwatchernew.R
-import com.example.futbinwatchernew.Utils.Error
-import com.example.futbinwatchernew.Utils.ErrorType
+import com.example.futbinwatchernew.UI.ErrorHandling.Error
+import com.example.futbinwatchernew.UI.ErrorHandling.ErrorType
+import com.example.futbinwatchernew.Utils.*
+import javax.inject.Inject
 
 
-class ErrorFragment(private val error: Error, private val retryAction: (() -> Unit)?):Fragment() {
+class ErrorFragment(private val error: Error, private val retryAction:
+((context: Context, newToken:String) -> LiveData<NetworkResponse<Int>>)?):Fragment() {
 
+    @Inject
+    lateinit var customViewModelFactory: CustomViewModelFactory
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +33,11 @@ class ErrorFragment(private val error: Error, private val retryAction: (() -> Un
         savedInstanceState: Bundle?
     ): View? {
         return LayoutInflater.from(requireContext()).inflate(R.layout.error_view,container,false)
+    }
+
+    override fun onAttach(context: Context) {
+        FUTBINWatcherApp.component.inject(this)
+        super.onAttach(context)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,9 +51,25 @@ class ErrorFragment(private val error: Error, private val retryAction: (() -> Un
         tv_error_message.text = error.message
         val btn_retryButton = view.findViewById<Button>(R.id.retry_button)
         retryAction?.run {
+            val vm = ViewModelProvider(requireActivity(),customViewModelFactory).get(MainActivityViewModel::class.java)
+            val sharedPrefRepo = SharedPrefRepo(requireActivity(), SharedPrefFileNames.CLIENT_REGISTRATION)
+            val token = sharedPrefRepo.readFromSharedPref(SharedPrefsTags.FIREBASE_TOKEN_KEY)
+                    as String
             btn_retryButton.visibility = View.VISIBLE
             btn_retryButton.setOnClickListener{
-                this.invoke()
+                this.invoke(requireContext(), token).observe(requireActivity(), Observer {response ->
+                    when (response) {
+                        is NetworkResponse.Success -> {
+                            sharedPrefRepo.writeToSharedPref(SharedPrefsTags.CLIENT_ID, response.data)
+                            sharedPrefRepo.writeToSharedPref(SharedPrefsTags.IS_DATABASE_IN_SYNC, true)
+                            vm.clientId = response.data
+                            parentFragmentManager.beginTransaction()
+                                .replace(R.id.fragment_container_view_tag,
+                                    SearchedPlayersFragment())
+                                .commit()
+                        }
+                    }
+                })
             }
         }
 
