a136c1742c9e948b7bea91ca1272c5f16533e827
VedantMehta39 <42540089+VedantMehta39@users.noreply.github.com>
2020-07-21 04:22:32 -0700

    Update SwipeToDeleteTrackedPlayerCallback.kt to delete directly from server

--- app/src/main/java/com/example/futbinwatchernew/UI/SwipeToDeleteTrackedPlayerCallback.kt
+++ app/src/main/java/com/example/futbinwatchernew/UI/SwipeToDeleteTrackedPlayerCallback.kt
@@ -4,16 +4,16 @@ package com.example.futbinwatchernew.UI
 
 
 import android.view.View
+import androidx.lifecycle.*
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
-import com.example.futbinwatchernew.Network.ResponseModels.PlayerTrackingRequest
+import com.example.futbinwatchernew.Utils.NetworkResponse
 import com.google.android.material.snackbar.Snackbar
-import java.util.*
-import kotlin.collections.ArrayList
 
 class SwipeToDeleteTrackedPlayerCallback(
     var adapter: TrackedPlayersRecyclerViewAdapter,
-    val view: View, var deletedPlayers: Stack<PlayerTrackingRequest>
+    private val view: View, private val owner:LifecycleOwner, private val deleteFromServer: (playerId:Int) -> LiveData<NetworkResponse<Int>>
 ):ItemTouchHelper.SimpleCallback (0,ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
     override fun onMove(
         recyclerView: RecyclerView,
@@ -27,16 +27,20 @@ class SwipeToDeleteTrackedPlayerCallback(
         val position = viewHolder.adapterPosition
 
         val deletedItem = adapter.data[position]
-        deletedPlayers.add(deletedItem)
-        (adapter.data as ArrayList).removeAt(position)
-        adapter.notifyDataSetChanged()
-        Snackbar.make(view, deletedItem.Player!!.CardName+" Deleted", Snackbar.LENGTH_LONG)
-            .setAction("Undo") { _ ->
-                (adapter.data as ArrayList).add(position, deletedItem)
-                deletedPlayers.remove(deletedItem)
-                adapter.notifyDataSetChanged()
+
+        deleteFromServer.invoke(deletedItem.PlayerId).observe(owner,Observer{response ->
+            when(response){
+                is NetworkResponse.Success ->{
+                    Snackbar.make(view, deletedItem.Player!!.CardName+" Deleted",
+                        Snackbar.LENGTH_LONG).show()
+                }
+                is NetworkResponse.Failure ->{
+                    Snackbar.make(view, deletedItem.Player!!.CardName+" could not be deleted." +
+                            " Please try again later!", Snackbar.LENGTH_LONG).show()
+                }
             }
-            .show()
+        })
+
     }
 
 }
