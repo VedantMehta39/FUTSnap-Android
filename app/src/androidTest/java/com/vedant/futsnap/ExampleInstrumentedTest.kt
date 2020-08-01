ab3205d9539623f920d04cb3e75edb0ed1703e9b
VedantMehta39 <42540089+VedantMehta39@users.noreply.github.com>
2020-08-01 05:58:47 -0700

    Change Package and Project Name

--- app/src/androidTest/java/com/vedant/futsnap/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/vedant/futsnap/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.futbinwatchernew", appContext.packageName)
+        assertEquals("com.vedant.futsnap", appContext.packageName)
     }
 }
