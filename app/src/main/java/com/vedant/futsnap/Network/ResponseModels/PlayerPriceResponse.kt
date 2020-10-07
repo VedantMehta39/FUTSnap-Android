c9165b7e21cff84c2746f67a8142a089df2ec560
VedantMehta39 <42540089+VedantMehta39@users.noreply.github.com>
2020-10-07 07:35:16 -0700

    Delete PlayerPriceResponse.kt since it's not needed anymore

--- app/src/main/java/com/vedant/futsnap/Network/ResponseModels/PlayerPriceResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.vedant.futsnap.Network.ResponseModels
-
-import com.google.gson.annotations.SerializedName
-
-data class PlayerPriceResponse(@SerializedName("ID") val id:Int,@SerializedName("LCPrice") val price:Int)
-
-data class PlayerPriceWrapperResponse(val data:List<PlayerPriceResponse>, val errorcode:String, val errormsg:String)
